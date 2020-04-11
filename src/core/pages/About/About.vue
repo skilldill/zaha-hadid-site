09fc2a14f70a53f04366f6c62e6e960f0aabfb0b
Alexander Lomonosov <skilldill@yandex.ru>
2020-04-11 18:57:23 +0500

    add about page

--- src/core/pages/About/About.vue
+++ src/core/pages/About/About.vue
@@ -1,6 +1,27 @@
 <template>
     <div class="about">
-        <h1>About</h1>
+        <h1 class="title">About us</h1>
+        <div class="top-picture">
+            <img src="../../../assets/about/about_header.png" alt="zaha hadid building">
+        </div>
+        <div class="content">
+            <div class="part part-text">
+                <p>    
+                    We work at all scales and in all sectors.
+                    We create transformative cultural, corporate, residential
+                    and other spaces that work in synchronicity with their surroundings.
+                </p>
+                <p>
+                    950 projects 44 countries
+                </p>
+                <p>
+                    Welcome to the interactive archive of Zaha Hadid Architects.
+                </p>
+            </div>
+            <div class="part">
+                <img src="../../../assets/about/about_content.png" alt="zaha hadid building">
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -9,5 +30,34 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.about {
+    padding-bottom: 80px;
 
+    .title {
+        font-weight: normal;
+        font-size: 150px;
+        margin-bottom: 47px;
+        text-align: right;
+        margin-right: 152px;
+    }
+
+    .content {
+        margin-top: 110px;
+        display: flex;
+
+        .part {
+            flex: 1;
+
+            p {
+                width: 600px;
+                font-size: 36px;
+                line-height: 35px;
+            }
+        }
+
+        .part-text {
+            padding-top: 20px;
+        }
+    }
+}
 </style>
