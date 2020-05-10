3c8af80449794531dde039b10cd58442e07d5273
Alexander Lomonosov <skilldill@yandex.ru>
2020-05-10 23:28:15 +0500

    add design page

--- src/core/pages/Projects/pages/SingleDesign/SingleDesign.vue
+++ src/core/pages/Projects/pages/SingleDesign/SingleDesign.vue
@@ -3,6 +3,37 @@
         <div class="title">
             <app-run-stroke :text="design.name"/>
         </div>
+        <div class="header">
+            <p>{{design.additional.description}}</p>
+            <div class="poster">
+                <img 
+                    :src="design.additional.poster" 
+                    :alt="design.name"
+                >
+            </div>
+        </div>
+        <div class="middle">
+            <div class="picture">
+                <img 
+                    :src="design.additional.photoMiddle" 
+                    :alt="design.name"
+                >
+            </div>
+        </div>
+        <div class="bottom">
+            <div class="coordinates">
+                <p
+                    v-for="(item, index) in design.additional.coordinates"
+                    :key="index"
+                >{{item}}</p>
+            </div>
+            <div class="picture">
+                <img 
+                    :src="design.additional.photoBottom" 
+                    :alt="design.name"
+                >
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -42,6 +73,53 @@ export default {
         height: 300px;
         margin-top: 200px;
         display: flex;
+        transform: translate(-100px);
+        width: calc(100% + 200px);
+    }
+
+    .header {
+        margin-top: 83px;
+        display: flex;
+        justify-content: space-between;
+
+        .poster {
+            margin-top: 320px;
+            transform: translateX(100px);
+        }
+
+        p {
+            width: 614px;
+            font-size: 36px;
+            line-height: 40px;
+            margin: 0;
+            padding: 0;
+        }
+    }
+
+    .middle {
+        margin-top: 100px;
+    }
+
+    .bottom {
+        position: relative;
+        width: 100%;
+        margin-top: 132px;
+        margin-bottom: 100px;
+        display: flex;
+        justify-content: flex-end;
+        
+        .coordinates {
+            position: absolute;
+            bottom: 0;
+            left: 0;
+
+            p {
+                margin: 0;
+                color: #767676;
+                font-size: 16px;
+                line-height: 24px;
+            }
+        }
     }
 }
 </style>
--- src/store/design/state.js
+++ src/store/design/state.js
@@ -4,7 +4,18 @@ export const designState = {
         {
             name: "Cirrus",
             picture: require("../../assets/projects/design/cirrus.png"),
-            place: "Cincinnati, USA"
+            place: "Cincinnati, USA",
+            additional: {
+                description: "Cirrus was installed five years after the completion of the seminal Lois & Richard Rosenthal Center for Contemporary Art in Cincinnati. Guiding the flow of circulation around the suspended galleries, its striated articulations unravel from the wall to the ground. The resulting series of voids give the sculpture its structure and define niches for sitting, reclining, leaning or lying.",
+                poster: require('../../assets/projects/design/cirrus/poster.png'),
+                photoMiddle: require('../../assets/projects/design/cirrus/photo1.png'),
+                photoBottom: require('../../assets/projects/design/cirrus/photo2.png'),
+                coordinates: [
+                    'Cincinnati, USA',
+                    'Lois & Richard Rosenthal Center',
+                    'for Contemporary Art'
+                ]
+            }
         },
         {
             name: "Aura-Villa Malcontenta",
